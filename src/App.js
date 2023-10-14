1304f959ec77c11506a8729d3bec267d63c6552c
ketki kelkar <ketkikelkar11@gmail.com>
2023-10-14 18:31:13 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import Navbar from "./Navbar";
+import About from "./About";
+import Hero from "./Hero";
+import Tech from "./Tech";
+import Projects from "./Projects";
+import Education from "./Education";
+import Experience from "./Experience";
+import Contact from "./Contact";
+
 import './App.css';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="relative z-0">
+        <div>
+          <Navbar />
+          <Hero /> 
+        </div>
+
+        <div className="bg-about bg-cover bg-center bg-no-repeat">
+          <About />
+        </div>
+
+        <div className="bg-tech bg-cover bg-center bg-no-repeat pb-10">
+          <Tech />
+        </div>
+
+        <Projects />
+
+        <div
+          className="bg-experience bg-cover bg-center bg-no-repeat 
+            rounded-tl-[150px] rounded-br-[150px]">
+          <div
+            className="bg-experienceLight bg-cover bg-center 
+            bg-no-repeat rounded-tl-[150px] rounded-br-[130px]">
+              <Education/>
+            <Experience />
+          </div>
+        </div>
+        <div className="relative z-0">
+          <Contact />
+        </div> 
+      </div>
+    </>
   );
-}
+};
 
 export default App;
